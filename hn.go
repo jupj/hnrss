0a06e667224b442dd5075322d2e5c3b1076c5ef5
Johan Jansson <johan.jansson@iki.fi>
2013-07-28 11:18:10 +0300

    First version of HN Top Links RSS feed

---
+++ hn.go
@@ -0,0 +1,120 @@
+package main
+
+import (
+	"encoding/xml"
+	"fmt"
+	"html"
+	"io"
+	"io/ioutil"
+	"net/http"
+	"os"
+	"regexp"
+)
+
+var _ = os.Stdout
+
+type rssItem struct {
+	Title string `xml:"title"`
+	Link  string `xml:"link"`
+	Comments string `xml:"comments"`
+    Guid string `xml:"guid"`
+	Description string `xml:"description"`
+}
+
+type rss struct {
+	XMLName     xml.Name  `xml:"rss"`
+	Version     string    `xml:"version,attr"`
+	Title       string    `xml:"channel>title"`
+	Link        string    `xml:"channel>link"`
+	Description string    `xml:"channel>description"`
+	Items       []rssItem `xml:"channel>item"`
+}
+
+func (r *rss) print(w io.Writer) {
+	// Writes the xml of the rssfeed to w
+	enc := xml.NewEncoder(w)
+	enc.Indent("", "  ")
+	if err := enc.Encode(r); err != nil {
+		fmt.Printf("error: %v\n", err)
+	}
+}
+
+func getHNsourceFile() string {
+    // Read the Hacker News html from a file
+	//htmlBytes, err := ioutil.ReadFile("hn.html")
+	htmlBytes, err := ioutil.ReadFile("best.htm")
+	if err != nil {
+		fmt.Println(err)
+		return ""
+	}
+	return html.UnescapeString(string(htmlBytes))
+}
+
+func getHNsourceHttp() string {
+    // Read the Hacker News html from the web
+    resp, err := http.Get("https://news.ycombinator.com/best")
+    if err != nil {
+		fmt.Println(err)
+		return ""
+    }
+    defer resp.Body.Close()
+    body, err := ioutil.ReadAll(resp.Body)
+    if err != nil {
+		fmt.Println(err)
+		return ""
+    }
+	return html.UnescapeString(string(body))
+}
+
+func parseHnHtmlToRss(html string) (rssfeed *rss) {
+    // Parses the Hacker News html and returns an rssfeed
+
+	// Example HN link:
+    /*
+<tr><td class="title" align="right" valign="top">30.</td><td><center><a id="up_5802043" href="https://news.ycombinator.com/vote?for=5802043&amp;dir=up&amp;whence=%62%65%73%74"><img src="hn_files/grayarrow.gif" border="0" hspace="2" vspace="3"></a><span id="down_5802043"></span></center></td><td class="title"><a href="http://www.bchanx.com/logos-in-pure-css-demo">Show HN: Logos in Pure CSS</a><span class="comhead"> (bchanx.com) </span></td></tr><tr><td colspan="2"></td><td class="subtext"><span id="score_5802043">267 points</span> by <a href="https://news.ycombinator.com/user?id=bchanx">bchanx</a> 4 days ago  | <a href="https://news.ycombinator.com/item?id=5802043">74 comments</a></td></tr>
+    */
+
+    re := regexp.MustCompile(`<td class="title"><a href="(?P<url>[^"]*)">(?P<desc>[^<]*)</a>(?:<span class="comhead">(?P<domain>[^<]*))?.*?href="item\?id=(?P<id>\d+)"`)
+
+
+	rssfeed = &rss{Version: "2.0",
+		Title:       "Hacker News Top Links",
+		Link:        "https://news.ycombinator.com/best",
+		Description: "Links for the intellectually curious, ranked by readers."}
+
+	//matches := re.FindAllStringSubmatch(html, -1)
+	matches := re.FindAllStringSubmatch(html, -1)
+	for _, match := range matches {
+		if len(match) > 0 {
+			url := match[1]
+			desc := match[2]
+			domain := match[3]
+            id := match[4]
+            comments := fmt.Sprintf("https://news.ycombinator.com/item?id=%s", id)
+			rssfeed.Items = append(rssfeed.Items, rssItem{fmt.Sprintf("%s%s", desc, domain),
+			url,
+			comments,
+			id,
+			fmt.Sprintf("<p><a href=\"%s\">%s</a> %s<p/><p><a href=\"%s\">Comments</a></p>", url, desc, domain, comments)})
+		}
+	}
+    return
+}
+
+func printHnRss(w io.Writer) {
+    // Prints the Hacker News RSS feed to w
+	rssfeed := parseHnHtmlToRss(getHNsourceHttp())
+	rssfeed.print(w)
+
+}
+
+func handler(w http.ResponseWriter, r *http.Request) {
+    //fmt.Fprintf(w, "Hi there, I love %s!", r.URL.Path[1:])
+    printHnRss(w)
+}
+
+func main() {
+    fmt.Println("Listening on localhost:8080")
+
+    http.HandleFunc("/", handler); http.ListenAndServe(":8080", nil)
+}
